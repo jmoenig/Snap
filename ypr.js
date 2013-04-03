ae21e364cb477a8a61e4e869c1b017835d3c5b1f
jmoenig <jens@moenig.org>
2013-04-03 10:09:11 +0200

    Fix: Removed added textarea from YPR module
    No more text area in upper left corner of the Snap! IDE when converting
    a .ypr project

--- ypr.js
+++ ypr.js
@@ -8,6 +8,8 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+Last changed 2013-04-03 by Jens Moenig (disabled text area overlay)
+
 */
 
 var sb = (function (sb) {
@@ -1429,9 +1431,9 @@ var sb = (function (sb) {
 				n('blocks', {}, nsCustomBlocks(project.stage.customBlocks, true))
 			]);
 			xml.toXML(out = []);
-            var d = document.createElement('textarea');
-            d.style.position = 'absolute';
-            document.body.appendChild(d).value = out.join('');
+            // var d = document.createElement('textarea');
+            // d.style.position = 'absolute';
+            // document.body.appendChild(d).value = out.join('');
 			return out.join('');
 		};
 
