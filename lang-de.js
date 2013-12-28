faa8b4fe2243b1a8ef5cc4342183f4880b728a3d
Michael Ball <cycomachead@gmail.com>
2013-12-27 18:49:56 -0800

    Add a 'get current DATE' block with various options, add some translations to FR and DE

--- lang-de.js
+++ lang-de.js
@@ -1246,4 +1246,47 @@ SnapTranslator.dict.de = {
         'letztes',
     'any':
         'beliebiges'
+
+    // Date reporter dropdowns
+    'Month':
+        'Monat',
+    // Months of the Year
+    'January':
+        'Januar',
+    'February':
+        'Februar',
+    'March':
+        'März',
+    'April':
+        'April';
+    'May':
+        'Mai',
+    'June':
+        'Juni',
+    'July':
+        'Juli';
+    'August':
+        'August',
+    'September':
+        'September',
+    'October':
+        'Oktober';
+    'November':
+        'November',
+    'December':
+        'Dezember',
+    'Sunday':
+        'Sonntag';
+    'Monday':
+        'Montag',
+    'Tuesday':
+        'Dienstag',
+    'Wednesday':
+        'Mittwoch';
+    'Thursday':
+        'Donnerstag',
+    'Friday':
+        'Freitag',
+    'Saturday':
+        'Samstag';
 };
