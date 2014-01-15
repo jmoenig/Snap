31e3fb9bacb3c203ee106bd3328b3958724aec4a
Charles H <chathaway@logrit.com>
2014-01-15 15:28:23 -0500

    No idea. Why 500?

--- objects.js
+++ objects.js
@@ -2829,19 +2829,22 @@ SpriteMorph.prototype.forward = function (steps) {
 
 SpriteMorph.prototype.translate_percent = function (percent) {
     var dest, delta=radians(this.heading);
-    console.log(this.width());
+    //console.log("width: " + this.parent.width() * this.scale);
+    //console.log("height: " + this.parent.height() * this.scale);
     
     // Translate by scale*width*percent in the X
-    var newX = this.position().x + (this.width() * percent/100);
+    var newX = this.position().x + (this.parent.width() * percent/500) * this.scale;
     // Translate by scale*height*percent in the Y
-    //var newY = this.position().y + (this.height() * percent/100) * Math.sin(delta);
+    //var newY = this.position().y + (this.parent.height() * percent/100 * this.scale) * Math.sin(delta);
     var newY = this.position().y;
     // Apply distance angle calulation
-    console.log("Current angle " + delta);
-    console.log("Current {" + this.position().x + ", " + this.position().y + "}");
-    console.log("New {" + newX + ", " + newY + "}");
-    var dist = Math.sqrt(Math.pow(this.position().x-newX, 2)+Math.pow(this.position().y-newY, 2))
-    console.log("Moving: " + dist);
+    //console.log("Current angle " + delta);
+    //console.log("Current {" + this.position().x + ", " + this.position().y + "}");
+    //console.log("New {" + newX + ", " + newY + "}");
+    var dist = Math.sqrt(Math.pow(this.position().x-newX, 2))
+    console.log("( " + this.heading + ") @ (" + this.scale + ") Moving: " + dist);
+    //console.log("Move X " + (this.position().x-newX));
+    //console.log("Move Y " + (this.position().y-newY));
 
     if (percent >= 0) {
         dest = this.position().distanceAngle(dist, this.heading);
