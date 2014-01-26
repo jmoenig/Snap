4b9c3d9ed44bc32656f58d037e9c12934319acef
Michael Ball <cycomachead@gmail.com>
2014-01-26 04:25:36 -0800

    adjust block to no longer use strings, and remove unneeded code

--- lang-de.js
+++ lang-de.js
@@ -1248,45 +1248,7 @@ SnapTranslator.dict.de = {
         'beliebiges',
 
     // Date reporter dropdowns
-    'Month':
-        'Monat',
-    // Months of the Year
-    'January':
-        'Januar',
-    'February':
-        'Februar',
-    'March':
-        'März',
-    'April':
-        'April',
-    'May':
-        'Mai',
-    'June':
-        'Juni',
-    'July':
-        'Juli',
-    'August':
-        'August',
-    'September':
-        'September',
-    'October':
-        'Oktober',
-    'November':
-        'November',
-    'December':
-        'Dezember',
-    'Sunday':
-        'Sonntag',
-    'Monday':
-        'Montag',
-    'Tuesday':
-        'Dienstag',
-    'Wednesday':
-        'Mittwoch',
-    'Thursday':
-        'Donnerstag',
-    'Friday':
-        'Freitag',
-    'Saturday':
-        'Samstag',
+    'month':
+        'monat',
+
 };
