d30afd9caf03020dc7bdb1e2b520015d16a90a8c
Manuel Menezes de Sequeira <MMSequeira@gmail.com>
2014-03-09 21:59:32 +0000

    Add localization support for SymbolMorph menu items.

--- byob.js
+++ byob.js
@@ -2579,7 +2579,7 @@ InputSlotDialogMorph.prototype.symbolMenu = function () {
         myself = this;
     SymbolMorph.prototype.names.forEach(function (symbol) {
         symbols.push([
-            [new SymbolMorph(symbol, myself.fontSize, symbolColor), symbol],
+            [new SymbolMorph(symbol, myself.fontSize, symbolColor), localize(symbol)],
             '$' + symbol
         ]);
     });
