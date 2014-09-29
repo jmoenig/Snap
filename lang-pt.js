fb2be1680ef2075504f6e938c9269b7b86b963fb
jmoenig <jens@moenig.org>
2014-09-29 10:02:20 +0200

    Portuguese translation update, thanks, Manuel!

--- lang-pt.js
+++ lang-pt.js
@@ -185,7 +185,7 @@ SnapTranslator.dict.pt = {
     'translator_e-mail':
         'mmsequeira@gmail.com',
     'last_changed':
-        '2014-07-29',
+        '2014-09-29',
 
     // GUI
     // control bar:
