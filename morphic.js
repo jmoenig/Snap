fa4b069c1fa23699df146b0ac7a40538f68cf937
Gubolin <gubolin@fantasymail.de>
2014-10-04 13:37:29 +0200

    leave a mark on pen down (fix #209)

--- morphic.js
+++ morphic.js
@@ -4097,6 +4097,7 @@ PenMorph.prototype.forward = function (steps) {
 
 PenMorph.prototype.down = function () {
     this.isDown = true;
+    this.drawLine(this.center(), this.center());
 };
 
 PenMorph.prototype.up = function () {
