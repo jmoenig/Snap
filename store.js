2cee474cb676f9ad04d55cc25f8d3fc42c002cce
jmoenig <jens@moenig.org>
2014-11-24 11:08:12 +0100

    Fixed #34

--- store.js
+++ store.js
@@ -61,7 +61,7 @@ SyntaxElementMorph, Variable*/
 
 // Global stuff ////////////////////////////////////////////////////////
 
-modules.store = '2014-November-14';
+modules.store = '2014-November-24';
 
 
 // XML_Serializer ///////////////////////////////////////////////////////
@@ -261,7 +261,9 @@ SnapSerializer.prototype.watcherLabels = {
     yPosition: 'y position',
     direction: 'direction',
     getScale: 'size',
+    getTempo: 'tempo',
     getLastAnswer: 'answer',
+    getLastMessage: 'message',
     getTimer: 'timer',
     getCostumeIdx: 'costume #',
     reportMouseX: 'mouse x',
