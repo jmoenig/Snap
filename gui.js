9ff7d05992fb9bda39ce28bbef8e67409ff46992
karenarialin <karenarialin@gmail.com>
2015-02-05 18:52:39 -0800

    Added language auto detection feature
    On line 3451, added function this.getBrowserLanguage to detect the user's browser language. On line 1759, called getBrowserLanguage to automatically refresh and reload in the user's browser language.

--- gui.js
+++ gui.js
@@ -1756,11 +1756,7 @@ IDE_Morph.prototype.applySavedSettings = function () {
     }
 
     // language
-    if (language && language !== 'en') {
-        this.userLanguage = language;
-    } else {
-        this.userLanguage = null;
-    }
+    this.userLanguage = this.getBrowserLanguage();
 
     //  click
     if (click && !BlockMorph.prototype.snapSound) {
@@ -3451,6 +3447,17 @@ IDE_Morph.prototype.saveProjectsBrowser = function () {
 };
 
 // IDE_Morph localization
+
+IDE_Morph.prototype.getBrowserLanguage = function () {
+    var lang;
+    if (window.navigator.languages) {
+        lang = window.navigator.languages[0];
+    } else {
+        lang = (window.navigator.userLanguage || window.navigator.language);
+    }
+    lang = lang.substring(0, 2);
+    return lang;
+};
 
 IDE_Morph.prototype.languageMenu = function () {
     var menu = new MenuMorph(this),
