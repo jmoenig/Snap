33032f918e2bf4745cab73eebf33c5ca3895ab20
Brian Broll <brian.broll@vanderbilt.edu>
2015-03-25 22:03:22 +0000

    Added comments and some structure

--- server/server.js
+++ server/server.js
@@ -10,7 +10,38 @@ var io = require('socket.io').listen(app.listen(port));
 
 var onMsgReceived = function() {
     // Handle a WebSocket message from NetsBlocks
+    // TODO
 };
 
 io.sockets.on('connection', function(socket) {
+    // When the "register" block is used, the client
+    // will send a message with:
+    //      + gameId: GAME_UUID 
+    //      + playerId: PLAYER_ID
+    //
+    // The server will then find a game that does not have 
+    // the given role filled
+    socket.on('register', function(data) {
+        var gameId = data.gameId,
+            role = data.gameId;
+
+        // Add the socket to the given group
+        // TODO
+
+        // Broadcast the join message to all the members of the given game 
+        // instance and broadcast a join message from all current members 
+        // to the new guy
+        // TODO
+    });
+
+
+    /**
+     * When the "broadcast" block is used, the server will receive 
+     * a message labeled 'message' from the client.
+     *
+     * On receiving this message, the server will then broadcast this
+     * message to all the other members in the group.
+     * 
+     */
+    socket.on('message', function(data) {
 });
