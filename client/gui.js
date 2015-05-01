1fd427c08fcb47792227b526f713fe3e6d9fae84
Jens Mönig <jens@moenig.org>
2015-05-01 12:24:52 -0400

    Release!

--- client/gui.js
+++ client/gui.js
@@ -2614,7 +2614,7 @@ IDE_Morph.prototype.aboutSnap = function () {
         module, btn1, btn2, btn3, btn4, licenseBtn, translatorsBtn,
         world = this.world();
 
-    aboutTxt = 'Snap! 4.0\nBuild Your Own Blocks\n\n--- rc ---\n\n'
+    aboutTxt = 'Snap! 4.0\nBuild Your Own Blocks\n\n'
         + 'Copyright \u24B8 2015 Jens M\u00F6nig and '
         + 'Brian Harvey\n'
         + 'jens@moenig.org, bh@cs.berkeley.edu\n\n'
