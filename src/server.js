7fceb745e72aeefd4d324093300256be389cc21c
Brian Broll <brian.broll@vanderbilt.edu>
2015-05-11 13:34:36 -0500

    Restructured files

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ var express = require('express'),
     port = 8080,
     NetsBlocksServer = require('./NetsBlocksServer');
 
-app.use(express.static(__dirname + '/../..'));
+app.use(express.static(__dirname + '/client/'));
 
 app.get('/', function(req, res) {
     res.redirect('/snap.html');
