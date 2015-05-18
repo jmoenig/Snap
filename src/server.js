9dbae4601166a9464f39c4512ce06b38c2e4c151
Brian Broll <brian.broll@vanderbilt.edu>
2015-05-18 15:06:29 -0500

    Added command line options for group managers

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 'use strict';
 var express = require('express'),
     app = express(),
-    port = 8080,
+    argv = require('yargs').argv,
+    port = process.env.PORT || 8080,
+    wsPort = process.env.WS_PORT || 5432,
     NetsBlocksServer = require('./NetsBlocksServer');
 
+console.log('port:', port);
 app.use(express.static(__dirname + '/client/'));
 
 app.get('/', function(req, res) {
@@ -14,14 +17,33 @@ app.listen(port);
 
 console.log('NetsBlocks server listening on port '+port);
 
-// Parse cmd line options for group manager?
-// TODO
+// Parse cmd line options for group manager
+var fs = require('fs'),
+    path = require('path');
 
-var TwoPlayerTurn = require('./GroupManagers/TurnBasedManager'),
-    TwoPlayer = require('./GroupManagers/TwoPlayerManager');
+var getGroupManagerDict = function() {
+    var result = {},
+        files = fs.readdirSync(__dirname + '/GroupManagers'),
+        name;
 
-var opts = {port: 5432, 
-             path: '', 
-             GroupManager: TwoPlayer};
+    for (var i = files.length; i--;) {
+        if (path.extname(files[i]) === '.js') {
+            name = files[i].toLowerCase().replace('manager', '');
+            name = name.replace('.js', '');
+            result[name] = require('./GroupManagers/'+files[i]);
+        }
+    }
+    return result;
+};
+
+var GroupManagers = getGroupManagerDict(),
+    def = 'basic',
+    group = argv.g || def,
+    manager = GroupManagers[group.toLowerCase()] || GroupManagers[def];
+
+// Set the group manager
+var opts = {port: wsPort,
+            path: '',
+            GroupManager: manager};
 var nbApp = new NetsBlocksServer(opts);
 nbApp.start(opts);
