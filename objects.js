9bb97c02bf32cd244bc2e293fe9e4d4852feefc1
bthomson2 <bthomson2@cox.net>
2016-02-18 14:07:18 -0500

    Added functions and blocks for border color

--- objects.js
+++ objects.js
@@ -622,6 +622,23 @@ SpriteMorph.prototype.initBlocks = function () {
             spec: 'set border color to %n',
             defaults: [0]
         },
+		getBorderShade: {
+				"type": "reporter",
+				"category": "pen",
+				"spec": "border shade"
+		},
+		setBorderShade: {
+					"type": "command",
+					"category": "pen",
+					"spec": "set border shade to %n",
+					"defaults": [100]
+		},
+		changeBorderShade: {
+					"type": "command",
+					"category": "pen",
+					"spec": "change border shade by %n",
+					"defaults": [100]
+		},
         penBorderColor: {
             type: 'reporter',
             category: 'pen',
@@ -1965,6 +1982,9 @@ SpriteMorph.prototype.blockTemplates = function (category) {
         blocks.push(block('penSize'))
         blocks.push('-');
         blocks.push(block('setBorderHue'));
+		blocks.push(block('getBorderShade'));
+		blocks.push(block('setBorderShade'));
+		blocks.push(block('changeBorderShade'));
         blocks.push(watcherToggle('penBorderColor'))
         blocks.push(block('penBorderColor'))
         blocks.push('-');
@@ -2992,6 +3012,45 @@ SpriteMorph.prototype.changeHue = function (delta) {
 SpriteMorph.prototype.getBorderHue = function () {
     return this.borderColor.hsv()[0] * 100;
 };
+
+SpriteMorph.prototype.getBorderShade = (function () {
+	return function () {
+		return this.borderColor.hsv()[2] * 100;
+	};
+}());
+
+SpriteMorph.prototype.setBorderShade = (function () {
+	return function (num) {
+      var hsv = this.borderColor.hsv(),
+        x = this.xPosition(),
+        y = this.yPosition();
+			
+		//Num goes in 0-100 range. 0 is black, 50 is the unchanged hue, 100 is white
+		num = Math.max(Math.min(+num || 0, 100), 0) / 50;
+		hsv[1] = 1;
+		hsv[2] = 1;
+
+		if(num > 1) {
+			hsv[1] = (2 - num); //Make it more white
+		}
+		else {
+			hsv[2] = num; //Make it more black
+		}
+
+		this.borderColor.set_hsv.apply(this.borderColor, hsv);
+		if (!this.costume) {
+			this.drawNew();
+			this.changed();
+		}
+		this.gotoXY(x, y);
+	};
+}());
+
+SpriteMorph.prototype.changeBorderShade = (function () {
+	return function (delta) {
+		this.setBorderShade(this.getBorderShade() + (+delta || 0));
+	};
+}());
 
 SpriteMorph.prototype.setBorderHue = function (num) {
     var hsv = this.borderColor.hsv(),
