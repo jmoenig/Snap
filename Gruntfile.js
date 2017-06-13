4f65d97d127e0311a35c0769996e900a5c978523
Aidan Lane <aidan.lane@monash.edu>
2017-06-13 12:55:27 +1000

    Fixed issue on iOS iBooks
    Add a margin to the top for iBooks on iPad and iPhone, where a black
    bar can sit across the top, blocking access to the widget's controls.

--- Gruntfile.js
+++ Gruntfile.js
@@ -88,8 +88,8 @@ module.exports = function(grunt) {
 		}
 		else
 		{
-			console.log("File in base html not in child html!");
-			throw new Error();
+      console.log("File " + v + " in base html not in child html!");
+      //throw new Error(); -- workaround, allow this for now
 		}
 	  })
 	  
