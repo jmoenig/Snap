9fab5cdf1a8ec5f2bcfa58c21489b0747f9b0d6e
Jens Mönig <jens@moenig.org>
2017-09-26 15:38:42 +0200

    revert attempt to fix flicker on Safari
    because it doesn’t do anything

--- morphic.js
+++ morphic.js
@@ -11663,10 +11663,6 @@ WorldMorph.prototype.initEventListeners = function () {
         function () {
             if (myself.useFillPage) {
                 myself.fillPage();
-
-                // fillPage will resize worldCanvas, which clears it.
-                // so we redraw immediately, to prevent flickering
-                myself.updateBroken();
             }
         },
         false
