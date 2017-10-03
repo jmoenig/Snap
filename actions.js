7f5246548bd525175875677cec1bd2284a2c22d5
Brian Broll <brian.broll@gmail.com>
2017-10-03 14:38:38 -0500

    pass error to reject fn if action fails

--- actions.js
+++ actions.js
@@ -252,9 +252,12 @@ ActionManager.prototype.completeAction = function(err, result) {
 
     // Call 'success' or 'reject', if relevant
     if (action.user === this.id) {
-        var dictName = err ? '_onReject' : '_onAccept';
-        if (this[dictName][action.id]) {
-            this[dictName][action.id](result);
+        if (err) {
+            if (this._onReject[action.id]) {
+                this._onReject[action.id](err);
+            }
+        } else if (this._onAccept[action.id]){
+            this._onAccept[action.id](result);
         }
         delete this._onAccept[action.id];
         delete this._onReject[action.id];
