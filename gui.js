30ce4ee739a721593c520ccfaa86c50374688ae1
Jens Mönig <jens@moenig.org>
2017-10-22 01:10:15 +0200

    release

--- gui.js
+++ gui.js
@@ -75,7 +75,7 @@ isRetinaSupported, SliderMorph, Animation*/
 
 // Global stuff ////////////////////////////////////////////////////////
 
-modules.gui = '2017-October-20';
+modules.gui = '2017-October-22';
 
 // Declarations
 
@@ -3382,7 +3382,7 @@ IDE_Morph.prototype.aboutSnap = function () {
         module, btn1, btn2, btn3, btn4, licenseBtn, translatorsBtn,
         world = this.world();
 
-    aboutTxt = 'Snap! 4.1 - rc -\nBuild Your Own Blocks\n\n'
+    aboutTxt = 'Snap! 4.1\nBuild Your Own Blocks\n\n'
         + 'Copyright \u24B8 2017 Jens M\u00F6nig and '
         + 'Brian Harvey\n'
         + 'jens@moenig.org, bh@cs.berkeley.edu\n\n'
