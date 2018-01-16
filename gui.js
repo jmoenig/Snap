5850e9954ec3f395a31274426ced06f080d785d3
Brian Broll <brian.broll@vanderbilt.edu>
2018-01-16 13:11:35 -0600

    Don't update undo controls on toggling app mode. Closes #437 (#439)

--- gui.js
+++ gui.js
@@ -4812,7 +4812,6 @@ IDE_Morph.prototype.toggleAppMode = function (appMode) {
         if (world.keyboardReceiver instanceof ScriptFocusMorph) {
             world.keyboardReceiver.stopEditing();
         }
-        this.currentSprite.scripts.hideUndoControls();
     } else {
         this.setColor(this.backgroundColor);
         this.controlBar.setColor(this.frameColor);
@@ -4841,8 +4840,6 @@ IDE_Morph.prototype.toggleAppMode = function (appMode) {
                 }
             });
         }
-        // update undo controls
-        this.currentSprite.scripts.updateUndoControls();
     }
     this.setExtent(this.world().extent()); // resume trackChanges
 };
