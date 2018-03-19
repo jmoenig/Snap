18dedeb06d9a425ab43344ea5ba88e09e0435398
Jens Mönig <jens@moenig.org>
2018-03-19 08:32:30 +0100

    prepare minor release

--- gui.js
+++ gui.js
@@ -75,7 +75,7 @@ isRetinaSupported, SliderMorph, Animation, BoxMorph, MediaRecorder*/
 
 // Global stuff ////////////////////////////////////////////////////////
 
-modules.gui = '2018-March-14';
+modules.gui = '2018-March-19';
 
 // Declarations
 
@@ -3499,7 +3499,7 @@ IDE_Morph.prototype.aboutSnap = function () {
         module, btn1, btn2, btn3, btn4, licenseBtn, translatorsBtn,
         world = this.world();
 
-    aboutTxt = 'Snap! 4.1.2.6\nBuild Your Own Blocks\n\n'
+    aboutTxt = 'Snap! 4.1.2.7\nBuild Your Own Blocks\n\n'
         + 'Copyright \u24B8 2018 Jens M\u00F6nig and '
         + 'Brian Harvey\n'
         + 'jens@moenig.org, bh@cs.berkeley.edu\n\n'
