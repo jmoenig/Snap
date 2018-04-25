05aeeb3b7943ac89e445f3df26f17db588392740
jmoenig <jens@moenig.org>
2018-04-25 16:46:27 +0200

    only randomize position when shift-clicking on new turtle-sprite button

--- gui.js
+++ gui.js
@@ -2293,9 +2293,11 @@ IDE_Morph.prototype.addNewSprite = function () {
     this.stage.add(sprite);
 
     // randomize sprite properties
-    sprite.setHue(rnd.call(this, 0, 100));
-    sprite.setBrightness(rnd.call(this, 50, 100));
-    sprite.turn(rnd.call(this, 1, 360));
+    if (this.world().currentKey !== 16) { // shift-click
+        sprite.setHue(rnd.call(this, 0, 100));
+        sprite.setBrightness(rnd.call(this, 50, 100));
+        sprite.turn(rnd.call(this, 1, 360));
+    }
     sprite.setXPosition(rnd.call(this, -220, 220));
     sprite.setYPosition(rnd.call(this, -160, 160));
 
