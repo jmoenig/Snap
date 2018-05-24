338f7c72c7953b0c7ba1586b56bd93b62d6a692f
Brian Broll <brian.broll@vanderbilt.edu>
2018-05-24 09:11:58 -0500

    Added "Save Copy" integration test. Closes #586 (#587)

--- test/save.spec.js
+++ test/save.spec.js
@@ -1,4 +1,4 @@
-/*globals driver, expect*/
+/*globals driver, expect, SnapCloud*/
 describe('save', function() {
     this.timeout(10000);
     beforeEach(() => {
@@ -22,7 +22,15 @@ describe('save', function() {
         driver.click(saveBtn);
 
         // Wait for the save message
-        return driver.waitUntil(showingSaveMsg);
+        return driver.expect(
+            showingSaveMsg,
+            `Did not see save message after "Save"`
+        );
+    }
+
+    function getProjectList(projectDialog) {
+        return projectDialog.listField.listContents.children
+            .map(item => item.labelString);
     }
 
     function openProjectsBrowser() {
@@ -36,29 +44,35 @@ describe('save', function() {
 
         // Open the saved project
         const projectDialog = driver.dialog();
-        function getProjectListCount() {
-            return projectDialog.listField.listContents.children.length;
-        }
-
-        return driver.waitUntil(() => getProjectListCount() > 1)
+        return driver.waitUntil(() => getProjectList(projectDialog).length > 1)
             .then(() => projectDialog);
     }
 
     function openSavedProject(projectName) {
         // Open the project dialog
+        let projectDialog;
         return openProjectsBrowser()
-            .then(projectDialog => {
+            .then(dialog => {
+                projectDialog = dialog;
+                return driver.expect(
+                    () => getProjectList(projectDialog).includes(projectName),
+                    `Could not find ${projectName} in project list`
+                );
+            })
+            .then(() => {
                 const projectList = projectDialog.listField.listContents.children;
                 const listItem = projectList.find(item => item.labelString === projectName);
-
                 driver.click(listItem);
                 projectDialog.accept();
 
                 // Check for the if-else block
-                return driver.waitUntil(() => {
-                    const blockCount = driver.ide().currentSprite.scripts.children.length;
-                    return blockCount === 1;
-                });
+                return driver.expect(
+                    () => {
+                        const blockCount = driver.ide().currentSprite.scripts.children.length;
+                        return blockCount === 1;
+                    },
+                    `Did not see blocks after loading saved project`
+                );
             });
 
     }
@@ -96,7 +110,10 @@ describe('save', function() {
                 driver.keys(saveAsName);
                 driver.dialog().accept();
 
-                return driver.waitUntil(showingSaveMsg);
+                return driver.expect(
+                    showingSaveMsg,
+                    `Did not see save message after "Save As"`
+                );
             })
             .then(() => openProjectsBrowser())
             .then(projectDialog => {
@@ -137,14 +154,48 @@ describe('save', function() {
             .then(projectDialog => {
                 let projectList = projectDialog.listField.listContents
                     .children.map(child => child.labelString);
-                return driver.waitUntil(() => {
-                    projectList = projectDialog.listField.listContents
-                        .children.map(child => child.labelString);
-                    return projectList.includes(newName);
-                }).then(() => expect(projectList.includes(projectName)).toBe(false));
+                return driver
+                    .expect(
+                        () => {
+                            projectList = projectDialog.listField.listContents
+                                .children.map(child => child.labelString);
+                            return projectList.includes(newName);
+                        },
+                        `Could not find ${newName} in project list`
+                    )
+                    .then(() => expect(projectList.includes(projectName)).toBe(false));
             });
     });
 
-    // - "save a copy"
-    // TODO
+    describe('saveACopy', function() {
+        let username;
+
+        beforeEach(() => username = SnapCloud.username);
+        afterEach(() => SnapCloud.username = username);
+
+        it('should create "Copy of <project>" on saveACopy', function() {
+            const projectName = driver.ide().room.name;
+            const ide = driver.ide();
+            // make the user a collaborator
+            SnapCloud.username = 'test';
+
+            ide.room.collaborators.push(SnapCloud.username);
+            ide.room.ownerId = 'otherUser';
+
+            // Click the project menu
+            driver.click(ide.controlBar.projectButton);
+            const dialog = driver.dialog();
+            const saveACopyBtn = dialog.children.find(item => item.action === 'saveACopy');
+            driver.click(saveACopyBtn);
+            return driver.expect(showingSaveMsg, `Did not see save message after "Save Copy"`)
+                .then(() => openProjectsBrowser())
+                .then(projectDialog => {
+                    const copyName = `Copy of ${projectName}`;
+                    return driver.expect(
+                        () => getProjectList(projectDialog).includes(copyName),
+                        `Could not find copied project (${copyName})`
+                    );
+                });
+        });
+    });
 });
