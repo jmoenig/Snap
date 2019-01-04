3cb935e0ce7065031eee97d2d167d693b373d1eb
Hamid Zare <hamidzr@users.noreply.github.com>
2019-01-04 15:27:25 -0600

    simple test script to run the client tests fixes #760 (#761)
    * WIP adding a testing script
    
    * added a slight warning

---
+++ bin/test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo make sure there is no other server instances running pointing to the wrong client code
+
+function redirectClientCode()
+{
+  if [ ! -L src/browser ]; then
+    echo redirecting the client code
+    rm src/browser -rf
+    ln -s  ../../ src/browser
+  else
+    echo WARN: is already symlinked.
+  fi
+}
+
+if [ -d .server ]; then
+
+  cd .server
+  git clean -fd
+  git checkout .
+  git checkout master
+  git pull
+  redirectClientCode
+  npm i
+
+else # it's the first time
+
+  echo pulling the server
+  rm -rf .server
+  git clone https://github.com/NetsBlox/NetsBlox.git .server
+  cd .server
+  redirectClientCode
+  echo installing the required packages
+  npm i
+
+fi
+
+npm run test-client
