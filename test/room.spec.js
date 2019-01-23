81f71ec979d27130ee57da13af8f1c23f986a75a
Hamid Zare <hamidzr@users.noreply.github.com>
2019-01-23 17:21:04 -0600

    stale roleid when cloning roles (#772)
    * to async/await
    
    * WIP focus on this test
    
    * wait for role update before requesting new role
    
    * remove incorrectly pushed file

--- test/room.spec.js
+++ test/room.spec.js
@@ -127,21 +127,26 @@ describe('room', function() {
     });
 
     describe('duplicate', function() {
-        before(() => {
-            return driver.reset()
-                .then(() => driver.addBlock('forward'))
-                .then(() => {
-                    driver.selectTab('Room');
+        before(async () => {
+            await driver.reset();
+            await driver.addBlock('forward');
+            await driver.selectTab('Room');
 
-                    const roleName = driver.ide().projectName;
-                    const role = driver.ide().room.getRole(roleName);
+            await driver.expect(() => { // determine if the roleid update is recevied from the server
+                const roleName = driver.ide().projectName;
+                const role = driver.ide().room.getRole(roleName);
+                return (role.id.match(/-\d{12,15}/) !== null); // FIXME
+            }, 'didnt receive role update');
 
-                    // duplicate the role
-                    driver.click(role);
-                    const dupBtn = driver.dialog().buttons.children
-                        .find(btn => btn.action === 'createRoleClone');
-                    driver.click(dupBtn);
-                });
+            // get a handle of the current/only role
+            const roleName = driver.ide().projectName;
+            const role = driver.ide().room.getRole(roleName);
+
+            // duplicate the role
+            driver.click(role);
+            const dupBtn = driver.dialog().buttons.children
+                .find(btn => btn.action === 'createRoleClone');
+            driver.click(dupBtn);
         });
 
         it('should create a new role', function() {
