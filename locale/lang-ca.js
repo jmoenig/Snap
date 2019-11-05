1da23e56247248c0a3b0156aa1366557e0aeafe7
Joan Guillén i Pelegay <jguille2@xtec.cat>
2019-11-05 15:18:56 +0100

    Updating Catalan locale info

--- locale/lang-ca.js
+++ locale/lang-ca.js
@@ -185,7 +185,7 @@ SnapTranslator.dict.ca = {
     'translator_e-mail':
         'bernat@snap4arduino.rocks, jguille2@xtec.cat', // optional
     'last_changed':
-        '2019-10-25', // this, too, will appear in the Translators tab
+        '2019-11-05', // this, too, will appear in the Translators tab
 
     // GUI
     // control bar:
