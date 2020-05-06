f6790950031c5bed4e83a424bb05edb373a17d68
jmoenig <jens@moenig.org>
2020-05-06 16:12:40 +0200

    named "alpha"

--- src/gui.js
+++ src/gui.js
@@ -3851,7 +3851,7 @@ IDE_Morph.prototype.aboutSnap = function () {
         module, btn1, btn2, btn3, btn4, licenseBtn, translatorsBtn,
         world = this.world();
 
-    aboutTxt = 'Snap! 6.0.0 - dev -\nBuild Your Own Blocks\n\n'
+    aboutTxt = 'Snap! 6.0.0 - alpha -\nBuild Your Own Blocks\n\n'
         + 'Copyright \u24B8 2008-2020 Jens M\u00F6nig and '
         + 'Brian Harvey\n'
         + 'jens@moenig.org, bh@cs.berkeley.edu\n\n'
