e5ceca4560e2d2bb2aa3430d9a7924a21ab89cad
jmoenig <jens@moenig.org>
2020-05-28 13:52:26 +0200

    tweaked large check mark symbol rendering

--- src/symbols.js
+++ src/symbols.js
@@ -1063,9 +1063,11 @@ SymbolMorph.prototype.renderSymbolCheckMark = function (ctx, color) {
 
     ctx.fillStyle = color.toString();
     ctx.beginPath();
-    ctx.moveTo(0, h * 0.5);
+    ctx.moveTo(w * 0.2, h * 0.5);
     ctx.lineTo(w * 0.5, h);
+    ctx.lineTo(w * 0.8, h * 0.3);
     ctx.lineTo(w, 0);
+    ctx.lineTo(w * 0.8, h * 0.1);
     ctx.lineTo(w * 0.5, h * 0.65);
     ctx.closePath();
     ctx.fill();
