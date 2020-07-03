89311cdd727e944a394f6412633d6d82e6fd1935
R4356th <61620631+R4356th@users.noreply.github.com>
2020-07-04 01:14:07 +0600

    Update info and version date

--- src/locale.js
+++ src/locale.js
@@ -422,11 +422,11 @@ SnapTranslator.dict.bn = {
     'language_name':
         'বাংলা',
     'language_translator':
-        'Dr. Mokter Hossain',
+        'Dr. Mokter Hossain, Radman Siddiki',
     'translator_e-mail':
-        'mokter@gmail.com',
+        'mokter@gmail.com, radman.siddiki@outlook.com',
     'last_changed':
-        '2014-07-02'
+        '2020-07-04'
 };
 
 SnapTranslator.dict.kn = {
