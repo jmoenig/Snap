dc33f4ac6757a7039a522d39c9461c9dd418b603
Brian Broll <brian.broll@vanderbilt.edu>
2020-08-11 16:22:20 -0500

    Fix minor type in error message

--- src/blocks.js
+++ src/blocks.js
@@ -7625,7 +7625,7 @@ ScriptsMorph.prototype.scriptTarget = function () {
     if (editor) {
         return editor.target;
     }
-    throw new Error('script target bannot be found for orphaned scripts');
+    throw new Error('script target cannot be found for orphaned scripts');
 };
 
 
