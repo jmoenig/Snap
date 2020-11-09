193f76b24639b1c40c34a8dabf29de57f549ef89
Brian Broll <brian.broll@gmail.com>
2020-11-09 14:44:34 -0600

    Only check activeMenu if not list contents. Fixes #1129 (#1130)

--- src/morphic.js
+++ src/morphic.js
@@ -8465,8 +8465,7 @@ MenuMorph.prototype.destroy = function () {
     if (this.hasFocus) {
         this.world.keyboardFocus = null;
     }
-    const isActiveMenu = this.world.activeMenu === this;
-    if (!this.isListContents && isActiveMenu) {
+    if (!this.isListContents && this.world.activeMenu === this) {
         this.world.activeMenu = null;
     }
     MenuMorph.uber.destroy.call(this);
