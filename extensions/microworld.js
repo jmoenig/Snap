fbca30434b9b12d1d3f9b9a422371d8970492534
Zak Kolar <zak.kolar@gmail.com>
2021-07-19 10:18:36 -0500

    fix prefix

--- extensions/microworld.js
+++ extensions/microworld.js
@@ -1,7 +1,7 @@
 var ide = world.children.find(child => {
     return child instanceof IDE_Morph;
 }),
-    prefix = 'mw';
+    prefix = 'mw_';
 
 function unifiedPalette(){
     ide.setUnifiedPalette(true);
