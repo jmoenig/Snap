b8ee20562684b5aa17a616f1d9399c258d3561a2
Zak Kolar <zak.kolar@gmail.com>
2021-08-13 17:01:20 -0500

    Remote sounds

---
+++ extensions/remote-sounds.js
@@ -0,0 +1,34 @@
+var ide = world.children.find(child => {
+    return child instanceof IDE_Morph;
+}),
+    prefix = 'rs_';
+
+SnapExtensions.primitives.set(
+    prefix+'load_sound(url)',
+    (src, context) => {
+        ide.getURL(
+            src,
+            function (blob) {
+                var reader = new window.FileReader();
+                reader.readAsDataURL(blob);
+                reader.onloadend = function() {
+                    var base64 = reader.result;
+                    context.setVarNamed('base64', 'data:audio/ogg;base64,' + base64.split(',')[1]);
+                };
+            },
+            'blob'
+        );
+    }
+)
+
+SnapExtensions.primitives.set(
+    prefix+'create_sound(base64)',
+    base64 => new Sound(new Audio(base64))
+)
+
+SnapExtensions.primitives.set(
+    prefix+'stop_all_speech_synthesis',
+    () => {
+        window.speechSynthesis.cancel();
+    }
+)
