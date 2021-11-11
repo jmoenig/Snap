9ed25adfb520a4181848f5075fa8d7be39dc8a17
jmoenig <jens@moenig.org>
2021-11-11 08:42:29 +0100

    Update lang-ca.js

--- locale/lang-ca.js
+++ locale/lang-ca.js
@@ -433,10 +433,10 @@ SnapTranslator.dict.ca = {
         'augmenta en %n el color del llapis',
     'set pen color to %n':
         'fixa el color del llapis a %n',
-    'change pen %hsva by %n':
-        'augmenta %hsva del llapis en %n',
-    'set pen %hsva to %n':
-        'fixa %hsva del llapis a %n',
+    'change pen %clrdim by %n':
+        'augmenta %clrdim del llapis en %n',
+    'set pen %clrdim to %n':
+        'fixa %clrdim del llapis a %n',
     'change pen shade by %n':
         'augmenta en %n la intensitat del llapis',
     'set pen shade to %n':
@@ -447,10 +447,10 @@ SnapTranslator.dict.ca = {
         'fixa la mida del llapis en %n',
     'set background color to %clr':
         'fixa el color del fons a %clr',
-    'change background %hsva by %n':
-        'augmenta %hsva del fons en %n',
-    'set background %hsva to %n':
-        'fixa %hsva del fons a %n',
+    'change background %clrdim by %n':
+        'augmenta %clrdim del fons en %n',
+    'set background %clrdim to %n':
+        'fixa %clrdim del fons a %n',
     'stamp':
         'estampa',
     'fill':
