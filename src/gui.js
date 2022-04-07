91ee341d8be7bf9a5bbbe21497fb222955f69fae
Jesus Villalobos <jvillalobos@berkeley.edu>
2022-04-07 15:40:12 -0700

    added functionality for injecting sprites with their xml

--- src/gui.js
+++ src/gui.js
@@ -9628,6 +9628,51 @@ SpriteIconMorph.prototype.userMenu = function () {
         );
     }
     menu.addItem("export...", 'exportSprite');
+
+    function injectScriptPic(ide, xml, pic) {
+        const inbDelim = "Snap\tBlocks\tEmbedded";      // in-band XML delimeter
+
+        const crc32 = (str, crc) => {
+            let table = [...Array(256).keys()].map(it => 
+                [...Array(8)].reduce((cc) => 
+                (cc & 1) ? (0xedb88320 ^ (cc >>> 1)) : (cc >>> 1), it)
+                );
+            crc = [...str].reduce((crc, ch) => { 
+                return (crc >>> 8) ^ table[(crc ^ ch.charCodeAt(0)) & 0xff]
+            }, (crc ||= 0) ^ (-1));
+            return ( crc ^ (-1) ) >>> 0;
+        };
+        const arr2Str = (arr) => { 
+            return arr.reduce((res, byte) => res + String.fromCharCode(byte), '');
+        };
+        const int2BStr = (val) => { 
+            return arr2Str(Array.from(new Uint8Array(new Uint32Array( [val] ).buffer)).reverse());
+        };
+        const buildChunk = (data) => { 
+            let res = "iTXt" + data; 
+            return int2BStr(data.length) + res + int2BStr(crc32(res));
+        };
+
+        let parts = pic.toDataURL("image/png").split(","),
+            bPart = atob(parts[1]).split("");
+        let newChunk = buildChunk("Snap!_XML\0\0\0\0\0"+inbDelim+xml+inbDelim),
+            name = top?.definition?.spec || top.selector;
+        bPart.splice(-12, 0, ...newChunk);
+        parts[1] = btoa(bPart.join(""));
+        ide.saveFileAs(parts.join(','), 'image/png', name);
+    }
+
+    menu.addItem("sprite and xml...", 
+        () => {
+            var ide = this.parentThatIsA(IDE_Morph),
+                xml = this.object.toXMLString(),
+                pic = this.object.getImage();
+            
+            injectScriptPic(ide, xml, pic);
+        },
+        "export this sprite and its xml"
+    )
+    
     return menu;
 };
 
