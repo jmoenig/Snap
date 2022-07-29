d7e74953c7b5ed8088f7acd107b96846cc69991e
Brian Broll <brian.broll@gmail.com>
2022-07-28 20:17:30 -0500

    v1.32.0

--- src/store-ext.js
+++ src/store-ext.js
@@ -8,7 +8,7 @@ NetsBloxSerializer.uber = SnapSerializer.prototype;
 SnapSerializer.prototype.isSavingHistory = true;
 
 NetsBloxSerializer.prototype.appName = 'NetsBlox';
-NetsBloxSerializer.prototype.version = '1.31.4';
+NetsBloxSerializer.prototype.version = '1.32.0';
 NetsBloxSerializer.prototype.app = NetsBloxSerializer.prototype.appName + ' ' +
     NetsBloxSerializer.prototype.version + ', http://netsblox.org';
 
