3ce84a31aa80bcc1028376864c6bd0214dc77ebd
Devin Jean <emailcruzjean@yahoo.com>
2022-10-14 14:50:38 -0500

    Support explicit global category for RPCs (#1336)
    * support global category
    
    * change global to empty path

--- src/blocks-ext.js
+++ src/blocks-ext.js
@@ -325,39 +325,40 @@ StructInputSlotMorph.prototype.getFieldValue = function(fieldname, value, meta) 
 };
 
 InputSlotMorph.prototype.serviceNames = async function () {
-    var services = await Services.getServicesMetadata(),
-        hasAuthoredServices,
-        menuDict = {},
-        category,
-        subMenu,
-        name;
+    const services = await Services.getServicesMetadata();
+    let menuDict = {};
 
-    for (var i = services.length; i--;) {
-        name = services[i].name;
-        const url = services[i].url;
-        if (services[i].categories.length) {
-            for (var j = services[i].categories.length; j--;) {
-                category = services[i].categories[j];
-                subMenu = menuDict;
-                for (var c = 0; c < category.length; c++) {
-                    if (!subMenu[category[c]]) {
-                        subMenu[category[c]] = {};
-                    }
-                    subMenu = subMenu[category[c]];
-                }
-                subMenu[name] = url ? [url + '/' + name] : name;
+    for (let i = services.length; i--;) {
+        const {name, url, categories} = services[i];
+        const putGlobal = () => { menuDict[name] = url ? [url + '/' + name] : name; };
+
+        if (categories.length === 0) {
+            putGlobal();
+            continue;
+        }
+
+        for (let j = categories.length; j--;) {
+            const category = categories[j];
+            if (category.length === 0) {
+                putGlobal();
+                continue;
             }
-        } else {
-            menuDict[name] = url ? [url + '/' + name] : name;
+
+            let subMenu = menuDict;
+            for (const c of category) {
+                if (!subMenu[c]) { subMenu[c] = {}; }
+                subMenu = subMenu[c];
+            }
+            subMenu[name] = url ? [url + '/' + name] : name;
         }
     }
 
     menuDict = sortDict(menuDict);
 
-    hasAuthoredServices = SnapCloud.username && menuDict.Community &&
+    const hasAuthoredServices = SnapCloud.username && menuDict.Community &&
         menuDict.Community[SnapCloud.username];
     if (hasAuthoredServices) {
-        subMenu = {};
+        const subMenu = {};
         subMenu[SnapCloud.username] = menuDict.Community[SnapCloud.username];
         Object.keys(menuDict.Community).forEach(function(key) {
             if (key !== SnapCloud.username) {
