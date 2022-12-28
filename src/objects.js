ea8ef055a34448d74fecde0e73560e7532321645
Devin Jean <emailcruzjean@yahoo.com>
2022-12-28 12:49:33 -0600

    add native try/catch

--- src/objects.js
+++ src/objects.js
@@ -2598,6 +2598,9 @@ SpriteMorph.prototype.blockTemplates = function (category) {
         blocks.push(block('doReport'));
         blocks.push(block('doStopThis'));
         blocks.push('-');
+        blocks.push(block('doTryCatch'));
+        blocks.push(block('doThrow'));
+        blocks.push('-');
         blocks.push(block('doRun'));
         blocks.push(block('fork'));
         blocks.push(block('evaluate'));
@@ -8891,6 +8894,9 @@ StageMorph.prototype.blockTemplates = function (category) {
         blocks.push('-');
         blocks.push(block('doReport'));
         blocks.push(block('doStopThis'));
+        blocks.push('-');
+        blocks.push(block('doTryCatch'));
+        blocks.push(block('doThrow'));
         blocks.push('-');
         blocks.push(block('doRun'));
         blocks.push(block('fork'));
