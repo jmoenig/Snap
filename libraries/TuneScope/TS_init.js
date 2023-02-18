7885443a7615cfb1ba8635c64d3b9ef4644d0811
harshpadhye <harshpadhye@gmail.com>
2023-02-18 12:50:23 -0500

    Added missing overdrive guitar and music box files

--- libraries/TuneScope/TS_init.js
+++ libraries/TuneScope/TS_init.js
@@ -129,6 +129,10 @@ window.parent.instrumentData = {
         path: "https://surikov.github.io/webaudiofontdata/sound/0260_JCLive_sf2_file.js",
         name: "_tone_0260_JCLive_sf2_file"
     },
+    "guitar, overdrive": {
+        path: "https://surikov.github.io/webaudiofontdata/sound/0291_LesPaul_sf2_file.js",
+        name: "_tone_0291_LesPaul_sf2_file"
+    },
     "piano": {
         path: "https://surikov.github.io/webaudiofontdata/sound/0020_JCLive_sf2_file.js",
         name: "_tone_0020_JCLive_sf2_file"
@@ -192,6 +196,10 @@ window.parent.instrumentData = {
     "marimba": {
         path: "https://surikov.github.io/webaudiofontdata/sound/0121_FluidR3_GM_sf2_file.js",
         name: "_tone_0121_FluidR3_GM_sf2_file"
+    },
+    "music box": {
+        path: "https://surikov.github.io/webaudiofontdata/sound/0100_SBLive_sf2.js",
+        name: "_tone_0100_SBLive_sf2"
     },
     "oboe": {
         path: "https://surikov.github.io/webaudiofontdata/sound/0680_JCLive_sf2_file.js",
