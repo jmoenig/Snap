5468ac56d32d6c141c1bbe659e2ed6c72faa8809
Brian Broll <brian.broll@gmail.com>
2023-03-27 20:01:11 -0500

    return cloud url at /cloud.txt path (#1361)

--- utils/build.js
+++ utils/build.js
@@ -1,30 +1,35 @@
 #! /usr/bin/env node
 
-import {fileURLToPath} from 'url';
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
-import dot from 'dot';
-import inquirer from 'inquirer';
-import fs from 'fs';
-import path from 'path';
-const indexTpl = dot.template(fs.readFileSync(path.join(__dirname, '..', 'index.dot')));
+import { fileURLToPath } from "url";
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+import dot from "dot";
+import inquirer from "inquirer";
+import fs from "fs";
+import path from "path";
+const indexTpl = dot.template(
+  fs.readFileSync(path.join(__dirname, "..", "index.dot")),
+);
 
 const questions = [
-    {
-        type: 'input',
-        name: 'cloud',
-        message: 'Enter a NetsBlox cloud URL',
-        default: 'http://localhost:7777'
-    },
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Enter the website title',
-        default: 'NetsBlox'
-    },
+  {
+    type: "input",
+    name: "cloud",
+    message: "Enter a NetsBlox cloud URL",
+    default: "http://localhost:7777",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "Enter the website title",
+    default: "NetsBlox",
+  },
 ];
 inquirer.prompt(questions)
-    .then(metaInfo => {
-        metaInfo.isDevMode = true;
-        const outfile = path.join(__dirname, '..', 'index.html');
-        fs.writeFileSync(outfile, indexTpl(metaInfo));
-    });
+  .then((metaInfo) => {
+    metaInfo.isDevMode = true;
+    const outfile = path.join(__dirname, "..", "index.html");
+    fs.writeFileSync(outfile, indexTpl(metaInfo));
+
+    const configFile = path.join(__dirname, "..", "cloud.txt");
+    fs.writeFileSync(configFile, metaInfo.cloud);
+  });
