8684f5d116b98c0505c095440c9c2694a4fee495
Zak Kolar <zakkolar@users.noreply.github.com>
2023-07-29 17:14:14 -0400

    add backwards compatibility

--- extensions/microworld.js
+++ extensions/microworld.js
@@ -281,6 +281,9 @@ SnapExtensions.primitives.set(
 SnapExtensions.primitives.set(
     prefix+'set_ide_lang(langCode,message,payload,loadingScreen)',
     function(langCode, message, payload,loadingScreen) {
+        // for backwards compatability, explicitly check that this is a boolean
+        // otherwise, the final param is proc which coerces to true
+        loadingScreen = typeof loadingScreen == "boolean" && loadingScreen;
         if(currentMicroworld()) {
             currentMicroworld().changeLanguage(langCode,message,payload,loadingScreen);
         }
