6bd59e97c0952f26196067c629350626bf274fc2
Brian Broll <brian.broll@gmail.com>
2023-10-03 11:32:23 -0500

    Update updateUrlQueryString; add unit tests. Fix #1429

---
+++ test/gui.spec.js
@@ -0,0 +1,106 @@
+/* globals expect, driver, assert */
+
+describe("gui", function () {
+  function setUrl(window, url) {
+      window.history.pushState("test", "test", url);
+  }
+
+  describe("public project", function () {
+    it("should set public URL", function () {
+      const project = {
+        owner: "test",
+        name: "PublicProject",
+        state: "Public",
+        saveState: "Saved",
+      };
+      driver.ide().updateUrlQueryString(project);
+
+      const { window } = driver.globals();
+      const url = window.location.href;
+      assert(url.includes("ProjectName=PublicProject"));
+      assert(url.includes("Username=test"));
+    });
+
+    it("should preserve hash", function () {
+      const { window } = driver.globals();
+      const initialHash = '#thisIsATest';
+      const initialUrl = window.location.origin + window.location.pathname +
+        '?' + initialHash;
+      setUrl(window, initialUrl);
+      const project = {
+        owner: "test",
+        name: "PublicProject",
+        state: "Public",
+        saveState: "Saved",
+      };
+      driver.ide().updateUrlQueryString(project);
+
+      const url = window.location.href;
+      assert(url.includes("ProjectName=PublicProject"));
+      assert(url.includes("Username=test"));
+      assert(url.includes(initialHash));
+    });
+
+    it("should preserve other query params", function () {
+      const { window } = driver.globals();
+      const queryParam = `extensions=${encodeURIComponent('["someUrl"]')}`;
+      const initialUrl = window.location.origin + window.location.pathname +
+        "?" + queryParam;
+      setUrl(window, initialUrl);
+
+      const project = {
+        owner: "test",
+        name: "PublicProject",
+        state: "Public",
+        saveState: "Saved",
+      };
+      driver.ide().updateUrlQueryString(project);
+
+      const url = window.location.href;
+      assert(url.includes("ProjectName=PublicProject"));
+      assert(url.includes("Username=test"));
+      assert(url.includes(queryParam));
+    });
+
+    it("should support hash & no query string", function () {
+      const { window } = driver.globals();
+      const initialHash = '#thisIsATest';
+      const initialUrl = window.location.origin + window.location.pathname +
+        initialHash;
+      setUrl(window, initialUrl);
+
+      const project = {
+        owner: "test",
+        name: "PublicProject",
+        state: "Public",
+        saveState: "Saved",
+      };
+      driver.ide().updateUrlQueryString(project);
+
+      const url = window.location.href;
+      assert(url.includes("ProjectName=PublicProject"));
+      assert(url.includes("Username=test"));
+      assert(url.includes(initialHash));
+    });
+  });
+
+  it("should clear project name, username if project is private", function () {
+      const { window } = driver.globals();
+      const initialHash = '#thisIsATest';
+      const initialUrl = window.location.origin + window.location.pathname +
+        '?action=present&ProjectName=PublicProject&Username=test' + initialHash;
+      setUrl(window, initialUrl);
+
+      const project = {
+        owner: "test",
+        name: "PrivateProject",
+        state: "Private",
+        saveState: "Saved",
+      };
+      driver.ide().updateUrlQueryString(project);
+
+      const url = window.location.href;
+      assert(!url.includes("Username=test"));
+      assert(url.includes(initialHash));
+  });
+});
