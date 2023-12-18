322362b2bf35791a8836f6234c82797ed1b69f9a
Brian Broll <brian.broll@gmail.com>
2023-12-18 16:04:05 -0600

    bump to v2.3.0

--- src/store-ext.js
+++ src/store-ext.js
@@ -8,7 +8,7 @@ NetsBloxSerializer.uber = SnapSerializer.prototype;
 SnapSerializer.prototype.isSavingHistory = true;
 
 NetsBloxSerializer.prototype.appName = 'NetsBlox';
-NetsBloxSerializer.prototype.version = '2.2.0';
+NetsBloxSerializer.prototype.version = '2.3.0';
 NetsBloxSerializer.prototype.app = NetsBloxSerializer.prototype.appName + ' ' +
     NetsBloxSerializer.prototype.version + ', http://netsblox.org';
 
