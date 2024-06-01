65e6be7302f142316cbc2cd85c45c8d6f35bcd64
Jens Mönig <jens@moenig.org>
2024-06-01 18:23:15 +0200

    incremented dev version

--- src/gui.js
+++ src/gui.js
@@ -91,7 +91,7 @@ modules.gui = '2024-June-01';
 
 // Declarations
 
-var SnapVersion = '10-240531-dev';
+var SnapVersion = '10-240601-dev';
 
 var IDE_Morph;
 var ProjectDialogMorph;
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var snapVersion = '10-240531-dev',
+var snapVersion = '10-240601-dev',
     cacheName = `snap-pwa-${snapVersion}`,
     filesToCache = [
         'snap.html',
