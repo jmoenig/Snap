ebd5a95763c10e826a986db282c3064ce5326093
Ashton(tripple blocked, owns illendo) <162846629+YeesterPlus@users.noreply.github.com>
2024-08-20 22:33:42 +0000

    fix

--- src/blocks.js
+++ src/blocks.js
@@ -5309,7 +5309,7 @@ BlockMorph.prototype.render = function (ctx) {
     this.cachedClrBright = this.bright();
     this.cachedClrDark = this.dark();
     var fill = this.color.copy()
-    fill.a = 0.1
+    fill.a = 0.25
     var cssfill = fill.toString();
 
     if (MorphicPreferences.isFlat) {
