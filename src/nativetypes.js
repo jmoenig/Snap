da18813683391dd89c177323976c356dae67fb80
Ashton(tripple blocked, owns illendo) <162846629+YeesterPlus@users.noreply.github.com>
2024-08-20 08:52:24 +0000

    fix

--- src/nativetypes.js
+++ src/nativetypes.js
@@ -1,23 +1,25 @@
-var SnapFunction = function (context){
-        SnapFunction.uber.constructor.apply(this,[])
-        this.getContext=()=>context
-        return new Proxy(this,{
-            apply:function(target,thisArg,args){
-                var stage = world.children[0].children[3]
-                var proc = new Process()
-                proc.receiver = thisArg||stage;
-                proc.initializeFor(context, new List(args));
-                proc.context.funct=target;
-                // proc.pushContext('doYield');
-                stage.threads.processes.push(proc);
-                proc.runStep();
-                return target.returnValue
-            }
-        })
+class SnapFunction extends Function {
+    constructor(context){
+        super()
+        return this.init(context)
     }
-    SnapFunction.prototype = Object.create(Function.prototype)
-    SnapFunction.uber = Function.prototype
-    SnapFunction.prototype.constructor = SnapFunction
-    SnapFunction.prototype.Return = function (value){
-        this.returnValue=value
-    }
+}
+SnapFunction.prototype.init = function(context){
+    this.getContext = () => context
+    return new Proxy(this, {
+        apply: function (target, thisArg, args) {
+            var stage = world.children[0].children[3]
+            var proc = new Process()
+            proc.receiver = thisArg || stage;
+            proc.initializeFor(context, new List(args));
+            proc.context.funct = target;
+            // proc.pushContext('doYield');
+            stage.threads.processes.push(proc);
+            proc.runStep();
+            return target.returnValue
+        }
+    })
+}
+SnapFunction.prototype.Return = function (value){
+    this.returnValue = value
+}
