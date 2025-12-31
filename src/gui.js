fe6d1e37fb2d1c7b5349d0b343a0cb4550149193
e016 <dcdc6894@gmail.com>
2025-12-31 10:21:23 -0700

    added bottom border to CorralStageMorph header

--- src/gui.js
+++ src/gui.js
@@ -96,7 +96,7 @@ modules.gui = "2025-November-23";
 // Declarations
 
 var SnapVersion = "11.0.8";
-var SplitVersion = "1.9.5";
+var SplitVersion = "1.9.6";
 
 var IDE_Morph;
 var ProjectDialogMorph;
@@ -14026,6 +14026,11 @@ CorralStageMorph.prototype.buildContents = function () {
     ctx.lineWidth = 2;
     ctx.fill();
     ctx.closePath();
+    ctx.beginPath()
+    ctx.moveTo(0, this.height()),
+    ctx.lineTo(this.width(), this.height())
+    ctx.closePath()
+    ctx.stroke()
     // ctx.beginPath();
 
     // ctx.moveTo(0, myself.corner / 2);
