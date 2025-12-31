2f1cacd41812c730047188c70475ec1cb777d2bd
e016 <dcdc6894@gmail.com>
2025-12-30 19:21:05 -0700

    1.9.0

--- src/gui.js
+++ src/gui.js
@@ -96,7 +96,7 @@ modules.gui = "2025-November-23";
 // Declarations
 
 var SnapVersion = "11.0.8";
-var SplitVersion = "1.8.1";
+var SplitVersion = "1.9.0";
 
 var IDE_Morph;
 var ProjectDialogMorph;
