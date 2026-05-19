e635bdf0ca21e69c8f344e5576b2a7ceb5c7ed35
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-05-19 17:10:38 -0600

    Add smaller screen fallback

--- src/blocks.js
+++ src/blocks.js
@@ -11707,10 +11707,63 @@ InputSlotMorph.prototype.keysMenu = function (searching) {
         this,
         this.fontSize
     );
+
     menu.popup(this.world(), new Point(
         this.right() - (menu.width() / 2),
         this.bottom()
     ));
+
+    // fallback for smaller screens
+    if (menu.width() > this.world().width()) {
+        menu.destroy()
+        return {
+            'any key' : ['any key'],
+            'up arrow': ['up arrow'],
+            'down arrow': ['down arrow'],
+            'right arrow': ['right arrow'],
+            'left arrow': ['left arrow'],
+            enter: ['enter'],
+            space : ['space'],
+            '+' : ['+'],
+            '-' : ['-'],
+            a : ['a'],
+            b : ['b'],
+            c : ['c'],
+            d : ['d'],
+            e : ['e'],
+            f : ['f'],
+            g : ['g'],
+            h : ['h'],
+            i : ['i'],
+            j : ['j'],
+            k : ['k'],
+            l : ['l'],
+            m : ['m'],
+            n : ['n'],
+            o : ['o'],
+            p : ['p'],
+            q : ['q'],
+            r : ['r'],
+            s : ['s'],
+            t : ['t'],
+            u : ['u'],
+            v : ['v'],
+            w : ['w'],
+            x : ['x'],
+            y : ['y'],
+            z : ['z'],
+            '0' : ['0'],
+            '1' : ['1'],
+            '2' : ['2'],
+            '3' : ['3'],
+            '4' : ['4'],
+            '5' : ['5'],
+            '6' : ['6'],
+            '7' : ['7'],
+            '8' : ['8'],
+            '9' : ['9']
+        };
+    }
     console.log('menu', menu.parent)
 };
 
